8ddc5800baa55d39c60f70005e1e546c4adb63c6
Rahmaatagortiii <91287751+Rahmaatagortiii@users.noreply.github.com>
2021-11-19 11:29:55 +0100

    projet

---
+++ my_project/src/Entity/Partenaire.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Partenaire
+ *
+ * @ORM\Table(name="partenaire")
+ * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\PartenaireRepository")
+ 
+ */
+class Partenaire
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_part", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idPart;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=20, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="des-soc", type="string", length=100, nullable=false)
+     */
+    private $desSoc;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=20, nullable=false)
+     */
+    private $adresse;
+
+    public function getIdPart(): ?int
+    {
+        return $this->idPart;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDesSoc(): ?string
+    {
+        return $this->desSoc;
+    }
+
+    public function setDesSoc(string $desSoc): self
+    {
+        $this->desSoc = $desSoc;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+
+}
